ffeb020e5fe3eec130d60c0b477d55c0885a3b2f
Admin <rbroker@digital.ai>
2020-12-17 15:05:11 -0500

    actions should apply the same yaml

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ then
 else
   curl -LO https://dist.xebialabs.com/public/xl-cli/$CLI_VERSION/linux-amd64/xl
   chmod +x xl
-  ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file petportalhosts.yaml --values version=$VERSION
+  ./xl apply --xl-deploy-url=$XLD_URL --xl-deploy-username=$XLD_USER --xl-deploy-password=$XLD_PASSWD --file windowshosts.yaml --values version=$VERSION
 fi
 rm artifacts/aws.ec2_instance.zip
 if [ -z "${GET_LOCAL}" ]
