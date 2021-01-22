e5fb601e20badfac7c178c5546d1665575fbf9da
Admin <rbroker@digital.ai>
2021-01-22 16:53:46 -0500

    update for windows password

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # PetPortalHosts
+
+
+## Notepad++
+
+```
+Invoke-WebRequest https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.8.8/npp.
+7.8.8.bin.zip -OutFile C:\Users\root\Downloads\npp.zip
+Expand-Archive -LiteralPath C:\Users\root\Downloads\npp.zip -DestinationPath "C:\Program Files\N
+otepad++"
+```
+
+## Firefox
+
+```
+Invoke-WebRequest "https://download.mozilla.org/?product=firefox-latest-ssl&os=win&lang=en-US" -OutFile C:\Users\root\Downloads\firefoxSetup.exe
+```
--- artifacts/aws.ec2_instance/windowsserver/windowsserver.tf
+++ artifacts/aws.ec2_instance/windowsserver/windowsserver.tf
@@ -34,7 +34,7 @@ Set-NetFirewallRule FPS-SMB-In-TCP -Enabled True
 Get-NetFirewallrule WINRM-HTTP-In-TCP | Get-NetFirewallAddressFilter | Set-NetFirewallAddressFilter -RemoteAddress Any
 Get-NetFirewallrule WINRM-HTTP-In-TCP-PUBLIC | Get-NetFirewallAddressFilter | Set-NetFirewallAddressFilter -RemoteAddress Any
 
-$myPassword = "Passw0rd!"
+$myPassword = "{{WINDOWS_PASSWORD}}"
 $myPasswordEnc = ConvertTo-SecureString -String $myPassword -AsPlainText -Force
 New-LocalUser -Name root -Description "Root User" -Password $myPasswordEnc –Verbose
 Set-LocalUser -Name root –PasswordNeverExpires $False
